9e4935d6027acf854a226dbe913cdb6b181d1bd6
Ian McElroy <mcelroyian@gmail.com>
2020-04-07 18:19:28 -0500

    8/10

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ for (i=0; i < navLinks.length; i++) {
 
 
 
-// On load append text to h1 with time
+// #1 On load append text to h1 with time
 let welcome = get('.intro h2')
 
 window.addEventListener('load', (e) => {
@@ -31,33 +31,94 @@ let paragraphs = getAll('p')
 
 for (i=0; i < paragraphs.length; i++) {
     
-    // mouseover: add border
+    // #3 mouseover: add border
     paragraphs[i].addEventListener('mouseover', function(e) {
         // console.log(this)
         // debugger
         this.style.border = "2px solid red"
     })
 
-    //mouseout - remove border to paragraphs
+    // #4 mouseout - remove border to paragraphs
     paragraphs[i].addEventListener('mouseout', function(e) {
         // console.log(this)
         // debugger
         this.style.border = null
     })
 
-    //dbclick: highlight text of element
-    paragraphs[i].addEventListener('dbclick', (e) => {
+    // #5 dbclick: highlight text of element
+    paragraphs[i].addEventListener('dblclick', (e) => {
         e.target.style.backgroundColor = 'yellow'
-        debugger
     })
 
 }
+
+
+// #6 hitting arrow up/down changes opacity
+
+let body = get('body')
+let opacity = 1
+document.addEventListener('keydown', (e) => {
+    
+    if (e.keyCode === 38) {
+        e.preventDefault()
+        // increae opacity
+        if (opacity < 1) {
+            opacity = opacity + .1
+        }
+        body.style.opacity = opacity;
+    } else if (e.keyCode === 40) {
+        e.preventDefault()
+        // decrease opacity
+        if (opacity > 0) {
+            opacity = opacity - .1
+        }
+        body.style.opacity = opacity;
+    }
+})
+
+
+// #7 Get value from clipboard and assign it a background color
+
+//Todo write a isHex function to check whether value is correct
+//begins with # etc
+
+const isHex = (text) => {
+    return true;
+}
+
+document.addEventListener('dblclick', (e) => {
+    navigator.clipboard.readText().then(
+        clipText => {
+            if (isHex(clipText)) {
+                console.log(`current hex is ${clipText}`)
+            document.querySelector("body").style.backgroundColor = clipText
+        } else {
+            console.log('not a hex');
+        }
+            })
+            
+        
+})
+
+// #8 Use mouse cordinates to make rgb values for background
+let header = get('header');
+document.addEventListener('mousemove', (e) => {
+    let r, g
+    r = e.screenX
+    g = e.screenY
+    header.style.backgroundColor = `rgb(${r},${g}, 100)`
+    console.log(`rgb (${r},${g}, 255)`)
+})
+
+
+
+
+
+
 /* 
 
 Events
     click
-    keydown
-    dbclick
     select
     focus
     cut
